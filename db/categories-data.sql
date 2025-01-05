a1205d07c5c193214e68e8760e3900d65eb68ef3
Fardeen Ansari <151157034+DeveloperFardeen@users.noreply.github.com>
2025-01-05 11:13:02 +0530

    Some predefined categories in  db

---
+++ db/categories-data.sql
@@ -0,0 +1,14 @@
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES
+(1, 'Productivity', 'Tools to help you manage time, tasks, and projects effectively.'),
+(2, 'Learning and Education', 'Platforms and resources for studying, note-taking, and skill-building.'),
+(3, 'Coding and Development', 'Resources for coding, debugging, and software development.'),
+(4, 'AI and Automation', 'AI-driven tools to automate tasks and enhance creativity.'),
+(5, 'Design and Creativity', 'Tools for graphic design, video editing, and creative content.'),
+(6, 'Research and Analysis', 'Tools to gather, organize, and analyze data effectively'),
+(7, 'Communication and Collaboration', 'Enhance teamwork with tools for communication and sharing'),
+(8, 'Language and Communication Skills', 'Tools to improve language skills and communication proficiency.');
